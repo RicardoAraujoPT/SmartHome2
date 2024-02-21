b86c88a7dce1fa835407c39ad4a706ab632009c4
PedroHGill <1231851@isep.ipp.pt>
2024-02-21 17:21:25 +0000

    Added Room class

---
+++ src/main/java/pt/ipp/isep/dei/examples/basic/domain/SmartHome/Domain/Room.java
@@ -0,0 +1,65 @@
+package pt.ipp.isep.dei.examples.basic.domain.SmartHome.Domain;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+public class Room {
+    private String _roomName;
+    private String _roomID;
+    private Integer _floorNumber;
+    private double _area;
+
+    private Double _height;
+    private List<Device> _devices = new ArrayList<>();
+
+    public Room(String roomName, Integer floorNumber, double area, Double height) throws InstantiationException{
+        if( !isValidConstructorArguments(roomName, floorNumber, area, height) ) {
+            throw (new InstantiationException("Invalid arguments"));
+        }
+        this._roomName = roomName;
+        this._roomID = UUID.randomUUID().toString();
+        this._floorNumber = floorNumber;
+        this._area = area;
+        this._height = height;
+    }
+    private boolean isValidConstructorArguments( String roomName, Integer floorNumber, double area, Double height) {
+        if (roomName == null || roomName.isEmpty()) {
+            return false;
+        }
+        if (floorNumber == null) {
+            return false;
+        }
+        if (area <= 0) {
+            return false;
+        }
+        if (height == null || height < 0) {
+            return false;
+        }
+        return true;
+    }
+
+    public String getRoomName() {
+        return _roomName;
+    }
+
+    public String getRoomID() {
+        return _roomID;
+    }
+
+    public Integer getFloorNumber() {
+        return _floorNumber;
+    }
+
+    public double getArea() {
+        return _area;
+    }
+
+    public Double getHeight() {
+        return _height;
+    }
+
+    public List<Device> getDevices() {
+        return new ArrayList<>(_devices);
+    }
+}
