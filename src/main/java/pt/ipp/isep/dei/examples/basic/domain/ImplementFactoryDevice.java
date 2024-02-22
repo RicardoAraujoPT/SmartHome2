df53cf22b67931896c57edab4c080a74cb289ea8
tiagopereiraswitch <1231861@isep.ipp.pt>
2024-02-22 10:42:47 +0000

    Alojamento -----> Device

--- src/main/java/pt/ipp/isep/dei/examples/basic/domain/ImplementFactoryDevice.java
+++ src/main/java/pt/ipp/isep/dei/examples/basic/domain/ImplementFactoryDevice.java
@@ -1,6 +1,6 @@
 package pt.ipp.isep.dei.examples.basic.domain;
 
-    public class ImplementFactoryAlojamento implements FactoryAlojamento {
+    public class ImplementFactoryDevice implements FactoryAlojamento {
     public Device createDevice(String deviceName) {
 
         return new Device(deviceName);
