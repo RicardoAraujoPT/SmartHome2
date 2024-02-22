6becb191b20071ac8d2b6bf8ae80a875dc1970a2
FranciscoRamosMartins <1231865@isep.ipp.pt>
2024-02-22 10:42:35 +0000

    ImplFactoryGPSCoordinates

--- src/main/java/pt/ipp/isep/dei/examples/basic/domain/SmartHome/Domain/FactoryGPSCoordinates.java
+++ src/main/java/pt/ipp/isep/dei/examples/basic/domain/SmartHome/Domain/FactoryGPSCoordinates.java
@@ -1,8 +1,6 @@
 package pt.ipp.isep.dei.examples.basic.domain.SmartHome.Domain;
 
-public class FactoryGPSCoordinates {
+public interface FactoryGPSCoordinates {
 
-        public GPSCoordinates createGPSCoordinates(double latitude, double longitude) {
-            return new GPSCoordinates(latitude, longitude);
-        }
+    public GPSCoordinates createGPSCoordinates(double latitude, double longitude);
 }
