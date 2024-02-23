da90849963ccb697b70a6399b680367f1c1a5d93
RicardoAraujo <147632200+RicardoAraujoSwitch@users.noreply.github.com>
2024-02-23 15:08:15 +0000

    Implemented Map for DeviceDTO to Domain

--- src/main/java/pt/ipp/isep/dei/examples/basic/domain/SmartHome/Mappers/DeviceDTOMapper.java
+++ src/main/java/pt/ipp/isep/dei/examples/basic/domain/SmartHome/Mappers/DeviceDTOMapper.java
@@ -1,15 +1,25 @@
 package pt.ipp.isep.dei.examples.basic.domain.SmartHome.Mappers;
 
 import pt.ipp.isep.dei.examples.basic.domain.SmartHome.DTO.DeviceDTO;
-import pt.ipp.isep.dei.examples.basic.domain.SmartHome.DTO.RoomDTO;
 import pt.ipp.isep.dei.examples.basic.domain.SmartHome.Domain.Device;
-import pt.ipp.isep.dei.examples.basic.domain.SmartHome.Domain.Room;
+import pt.ipp.isep.dei.examples.basic.domain.SmartHome.Domain.House;
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class DeviceDTOMapper {
+
+    private House _house;
+    private HashMap<DeviceDTO, Device> _mapDevicesDTO = new HashMap<>();
+
+    public HashMap<DeviceDTO, Device> getMapDevicesDTO() {
+        return new HashMap<>(_mapDevicesDTO);
+    }
+
+    public DeviceDTOMapper(House house) {
+        this._house = house;
+    }
 
     // Converts a DeviceDTO to a Device
     public static Device DTOToDevice(DeviceDTO deviceDTO) throws InstantiationException {
@@ -21,7 +31,12 @@ public class DeviceDTOMapper {
         return new DeviceDTO(device.getDeviceName(), device.getDeviceID());
     }
 
-    public Map<DeviceDTO, Device> DeviceMap_DTOAndDomain(List<Device> devices) {
+    //Converts a RoomDTO with name and status to a Room
+    public static DeviceDTO DeviceToDTOWithStatus(Device device) {
+        return new DeviceDTO(device.getDeviceName(), device.getDeviceIsActive());
+    }
+
+    public Map<DeviceDTO, Device> DeviceMap_DTOToDomain(List<Device> devices) {
 
         Map<DeviceDTO, Device> devicesDTOAndDevices = new HashMap<>();
 
