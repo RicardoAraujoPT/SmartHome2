62cdfcc739687e80098881b0d0acb0e27efa5d27
Francisco Martins <1231865@isep.ipp.pt>
2024-02-27 12:33:38 +0000

    Actuator and Sensor interfaces updated.

--- src/main/java/pt/ipp/isep/dei/examples/basic/domain/SmartHome/Domain/Actuator.java
+++ src/main/java/pt/ipp/isep/dei/examples/basic/domain/SmartHome/Domain/Actuator.java
@@ -1,4 +1,23 @@
 package pt.ipp.isep.dei.examples.basic.domain.SmartHome.Domain;
 
-public class Actuator {
+/**
+ * This interface defines the contract for an actuator in the SmartHome domain.
+ * Each actuator has an actuator type.
+ */
+public interface Actuator {
+
+    /**
+     * Gets the current value of the sensor.
+     *
+     * @return The value of the sensor.
+     */
+    public Value getValue();
+
+    /**
+     * Gets the type of the actuator.
+     *
+     * @return The type of the actuator.
+     */
+    // public ActuatorType getActuatorType();
+
 }
