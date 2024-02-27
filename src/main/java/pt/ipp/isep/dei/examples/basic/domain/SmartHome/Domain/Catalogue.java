52d8e5923eabfeade5eea1bcca213e5ad42dfc2e
Francisco Martins <1231865@isep.ipp.pt>
2024-02-27 15:44:12 +0000

    Catalogue now has actuators.

--- src/main/java/pt/ipp/isep/dei/examples/basic/domain/SmartHome/Domain/Catalogue.java
+++ src/main/java/pt/ipp/isep/dei/examples/basic/domain/SmartHome/Domain/Catalogue.java
@@ -14,11 +14,15 @@ import java.util.Optional;
 public class Catalogue {
     private List<SensorType> _listSensorTypes = new ArrayList<>();
     private List<String> _listStringClassesSensors;
+    private List <ActuatorType> _listActuatorTypes = new ArrayList<>();
+    private List <String> _listStringClassesActuators;
 
     public Catalogue(Configuration config) {
         // access configuration properties
         String[] arrayStringClassesSensors = config.getStringArray("sensor");
         this._listStringClassesSensors = List.of(arrayStringClassesSensors);
+        String[] arrayStringClassesActuators = config.getStringArray("actuator");
+        this._listStringClassesActuators = List.of(arrayStringClassesActuators);
     }
 
     public Catalogue(String filePathname) throws InstantiationException {
@@ -29,6 +33,8 @@ public class Catalogue {
             // access configuration properties
             String[] arrayStringClassesSensors = config.getStringArray("sensor");
             this._listStringClassesSensors = List.of(arrayStringClassesSensors);
+            String[] arrayStringClassesActuators = config.getStringArray("actuator");
+            this._listStringClassesActuators = List.of(arrayStringClassesActuators);
         } catch (ConfigurationException exception) {
             // Something went wrong
             throw new InstantiationException("something went wrong in reading the configuration: " + exception.getMessage());
@@ -61,6 +67,45 @@ public class Catalogue {
                 String strModelPath = "pt.ipp.isep.dei.examples.basic.domain.SmartHome." + strModel;
                 Sensor sensor = (Sensor) Class.forName(strModelPath).getConstructor(Catalogue.class).newInstance(this);
                 return sensor;
+            }
+            // due to the previous conditions, exception will not throw, but Class.forName... requires the catch
+            catch (ClassNotFoundException |
+                   NoSuchMethodException |
+                   InstantiationException |
+                   IllegalAccessException |
+                   InvocationTargetException e) {
+                return null;
+            }
+        } else
+            return null;
+    }
+
+    public ActuatorType addActuatorType(String strDescription, Unit unit) throws InstantiationException {
+        ActuatorType _actuator = new ActuatorType(strDescription, unit);
+
+        this._listActuatorTypes.add(_actuator);
+
+        return _actuator;
+    }
+
+    public ActuatorType getActuatorType(String strDescription) {
+        Optional<ActuatorType> optActuatorType = this._listActuatorTypes.stream().filter(a -> a.getDescription().equals(strDescription)).findFirst();
+
+        return optActuatorType.orElse(null);
+    }
+
+    public List<String> getActuatorModels() {
+        return new ArrayList<>(this._listStringClassesActuators);
+    }
+
+    public Actuator getActuator(String strModel) {
+        Optional<String> optActuatorType = this.getActuatorModels().stream().filter(a -> a.equals(strModel)).findFirst();
+
+        if (optActuatorType.isPresent()) {
+            try {
+                String strModelPath = "pt.ipp.isep.dei.examples.basic.domain.SmartHome." + strModel;
+                Actuator actuator = (Actuator) Class.forName(strModelPath).getConstructor(Catalogue.class).newInstance(this);
+                return actuator;
             }
             // due to the previous conditions, exception will not throw, but Class.forName... requires the catch
             catch (ClassNotFoundException |
