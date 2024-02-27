d825a27b1d37fc03f53a1d3b97a84cbcb15ed180
Pedro Gil <1231851@isep.ipp.pt>
2024-02-27 15:31:07 +0000

    Unnecessary class factories eliminated

--- src/main/java/pt/ipp/isep/dei/examples/basic/domain/SmartHome/Domain/FactorySensor.java
+++ src/main/java/pt/ipp/isep/dei/examples/basic/domain/SmartHome/Domain/FactorySensor.java
@@ -1,7 +1,20 @@
 package pt.ipp.isep.dei.examples.basic.domain.SmartHome.Domain;
-/*
-public interface FactorySensor {
-    Sensor createSensor(String strModel, Catalogue catalogue);
-}
 
-*/
+import java.lang.reflect.InvocationTargetException;
+
+public class FactorySensor {
+    public Sensor createSensor(String strModel, Catalogue catalogue) {
+        try {
+            Sensor sensor = (Sensor) Class.forName(strModel).getConstructor(Catalogue.class).newInstance(catalogue);
+            return sensor;
+        }
+        // due to the previous conditions, exception will not throw, but Class.forName... requires the catch
+        catch (ClassNotFoundException |
+               NoSuchMethodException |
+               InstantiationException |
+               IllegalAccessException |
+               InvocationTargetException e) {
+            return null;
+        }
+    }
+}
