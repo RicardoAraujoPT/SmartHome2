d1bb2bb584f0555b3882a62b2a6b76fa5e89454b
Maria Neto <1231842@isep.ipp.pt>
2024-02-28 20:31:04 +0000

    changed methods and tests in US02CreateRoomController to different house constructor. javadocs added in US02CreateRoomController

--- src/test/java/pt/ipp/isep/dei/examples/basic/domain/ControllerTest/US02CreateRoomControllerTest.java
+++ src/test/java/pt/ipp/isep/dei/examples/basic/domain/ControllerTest/US02CreateRoomControllerTest.java
@@ -3,15 +3,25 @@ package pt.ipp.isep.dei.examples.basic.domain.ControllerTest;
 import org.junit.jupiter.api.Test;
 import pt.ipp.isep.dei.examples.basic.domain.SmartHome.Controllers.US02CreateRoomController;
 import pt.ipp.isep.dei.examples.basic.domain.SmartHome.DTO.RoomDTO;
+import pt.ipp.isep.dei.examples.basic.domain.SmartHome.Domain.FactoryDevice;
+import pt.ipp.isep.dei.examples.basic.domain.SmartHome.Domain.FactoryLocation;
+import pt.ipp.isep.dei.examples.basic.domain.SmartHome.Domain.FactoryRoom;
 import pt.ipp.isep.dei.examples.basic.domain.SmartHome.Domain.House;
 
 import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Test class for US02CreateRoomController
+ */
 
 public class US02CreateRoomControllerTest {
     @Test
     void validArguments_shouldCreateValidRoom() throws InstantiationException {
         //Arrange
-        House myHouse = new House("address", "zipCode", 55.2, -2.25);
+        FactoryDevice factoryDevice = new FactoryDevice();
+        FactoryLocation factoryLocation = new FactoryLocation();
+        FactoryRoom factoryRoom = new FactoryRoom(factoryDevice);
+        House myHouse = new House(factoryLocation, factoryRoom, "address", "zipCode", 55.2, -2.25);
         US02CreateRoomController myController = new US02CreateRoomController(myHouse);
         RoomDTO roomDTO = new RoomDTO("Test Room", 0, 25.0, 3.2);
         //Act
@@ -23,7 +33,10 @@ public class US02CreateRoomControllerTest {
     @Test
     void nullRoomName_shouldThrowInstantiationException() throws InstantiationException {
         // Arrange
-        House myHouse = new House("address", "zipCode", 55.2, -2.25);
+        FactoryDevice factoryDevice = new FactoryDevice();
+        FactoryLocation factoryLocation = new FactoryLocation();
+        FactoryRoom factoryRoom = new FactoryRoom(factoryDevice);
+        House myHouse = new House(factoryLocation, factoryRoom, "address", "zipCode", 55.2, -2.25);
         US02CreateRoomController myController = new US02CreateRoomController(myHouse);
         String expectedMessage = "Invalid arguments";
         RoomDTO roomDTO = new RoomDTO(null, 0, 25.0, 3.2);
@@ -38,7 +51,10 @@ public class US02CreateRoomControllerTest {
     @Test
     void negativeArea_shouldThrowInstantiationException() throws InstantiationException {
         // Arrange
-        House myHouse = new House("address", "zipCode", 55.2, -2.25);
+        FactoryDevice factoryDevice = new FactoryDevice();
+        FactoryLocation factoryLocation = new FactoryLocation();
+        FactoryRoom factoryRoom = new FactoryRoom(factoryDevice);
+        House myHouse = new House(factoryLocation, factoryRoom, "address", "zipCode", 55.2, -2.25);
         US02CreateRoomController myController = new US02CreateRoomController(myHouse);
         String expectedMessage = "Invalid arguments";
         RoomDTO roomDTO = new RoomDTO("Test Room", 0, -25.0, 3.2);
@@ -53,7 +69,10 @@ public class US02CreateRoomControllerTest {
     @Test
     void negativeHeight_shouldThrowInstantiationException() throws InstantiationException {
         // Arrange
-        House myHouse = new House("address", "zipCode", 55.2, -2.25);
+        FactoryDevice factoryDevice = new FactoryDevice();
+        FactoryLocation factoryLocation = new FactoryLocation();
+        FactoryRoom factoryRoom = new FactoryRoom(factoryDevice);
+        House myHouse = new House(factoryLocation, factoryRoom, "address", "zipCode", 55.2, -2.25);
         US02CreateRoomController myController = new US02CreateRoomController(myHouse);
         String expectedMessage = "Invalid arguments";
         RoomDTO roomDTO = new RoomDTO("Test Room", 0, 25.0, -3.2);
