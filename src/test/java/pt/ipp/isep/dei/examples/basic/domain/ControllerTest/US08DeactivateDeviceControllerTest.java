91cad195cafe698603426a3993c477897aaec826
Ricardo Araujo <1231856@isep.ipp.pt>
2024-02-28 12:26:03 +0000

    Removed House attribute, added documentation, and NullRoom Test to US08DeactivateDeviceControllerTest Class

--- src/test/java/pt/ipp/isep/dei/examples/basic/domain/ControllerTest/US08DeactivateDeviceControllerTest.java
+++ src/test/java/pt/ipp/isep/dei/examples/basic/domain/ControllerTest/US08DeactivateDeviceControllerTest.java
@@ -1,28 +1,24 @@
 package pt.ipp.isep.dei.examples.basic.domain.ControllerTest;
 import org.junit.jupiter.api.Test;
-import pt.ipp.isep.dei.examples.basic.domain.SmartHome.Controllers.US06GetDevicesOfASpecificRoomController;
 import pt.ipp.isep.dei.examples.basic.domain.SmartHome.Controllers.US08DeactivateDeviceController;
-import pt.ipp.isep.dei.examples.basic.domain.SmartHome.Controllers.US09GetDevicesByTypeController;
 import pt.ipp.isep.dei.examples.basic.domain.SmartHome.DTO.DeviceDTO;
-import pt.ipp.isep.dei.examples.basic.domain.SmartHome.Domain.Catalogue;
-import pt.ipp.isep.dei.examples.basic.domain.SmartHome.Domain.Device;
 import pt.ipp.isep.dei.examples.basic.domain.SmartHome.Domain.House;
-import pt.ipp.isep.dei.examples.basic.domain.SmartHome.Domain.Room;
-import java.util.UUID;
-
 import static org.junit.jupiter.api.Assertions.*;
 
-
+/**
+ * This is a test class of the US08DeactivateDeviceController. It contains tests for:
+ * valid device;
+ * Non existing Device;
+ * Already inactive Device
+ * And non-existing House
+ */
 public class US08DeactivateDeviceControllerTest {
 
-    House _myHouse;
-
-    Room _myRoom;
 
     @Test
     void validDevice_shouldreturnFalse() throws InstantiationException {
         //Arrange
-        _myHouse = new House("zipCode", "Street", 55, 105);
+        House _myHouse = new House("zipCode", "Street", 55, 105);
         _myHouse.createRoom("roomName1", 0, 25, 2.5);
         _myHouse.getRoomByName("roomName1").createDevice("myDevice");
         US08DeactivateDeviceController myController = new US08DeactivateDeviceController(_myHouse);
@@ -37,12 +33,12 @@ public class US08DeactivateDeviceControllerTest {
     /**
      * This test checks if the deactivateDevice method correctly handles the case where the specified device does not exist.
      * The test creates a US08DeactivateDeviceController object and calls the deactivateDevice method with a non-existing device name.
-     * The expected result is that the method returns null, indicating that the device was not found.
+     * The expected result is that the method returns an exception.
      */
     @Test
     void nonExistingDevice_shouldReturnNull() throws InstantiationException {
         //Arrange
-        _myHouse = new House("zipCode", "Street", 55, 105);
+        House _myHouse = new House("zipCode", "Street", 55, 105);
         _myHouse.createRoom("roomName1", 0, 25, 2.5);
         _myHouse.getRoomByName("roomName1").createDevice("myDevice");
         US08DeactivateDeviceController myController = new US08DeactivateDeviceController(_myHouse);
@@ -61,7 +57,7 @@ public class US08DeactivateDeviceControllerTest {
     @Test
     void deactivatedDevice_shouldReturnFalse() throws InstantiationException {
         //Arrange
-        _myHouse = new House("zipCode", "Street", 55, 105);
+        House _myHouse = new House("zipCode", "Street", 55, 105);
         _myHouse.createRoom("roomName1", 0, 25, 2.5);
         _myHouse.getRoomByName("roomName1").createDevice("myDevice");
         US08DeactivateDeviceController myController = new US08DeactivateDeviceController(_myHouse);
@@ -81,11 +77,32 @@ public class US08DeactivateDeviceControllerTest {
      * The expected result is that the method should return an exception.
      */
     @Test
-    void nullHouse_shouldThrowIllegalArgumentException() throws IllegalArgumentException, InstantiationException {
+    void nullHouse_shouldThrowIllegalArgumentException() throws IllegalArgumentException {
         //Arrange
         String expectedMessage = "Invalid house";
         //Act
         Exception exception = assertThrows(IllegalArgumentException.class, () -> new US08DeactivateDeviceController(null));
+        String actualMessage = exception.getMessage();
+        //Assert
+        assertEquals(expectedMessage, actualMessage);
+    }
+
+    /**
+     * This test checks if the deactivateDevice method correctly handles the case where the room in which the device
+     * is located does not exist.
+     * The test creates a US08DeactivateDeviceController object and calls the deactivateDevice method with a device
+     * on a non-existing room.
+     * The expected result is that the method should return an exception.
+     */
+    @Test
+    void nullRoom_shouldThrowIllegalArgumentException() throws IllegalArgumentException {
+        //Arrange
+        House _myHouse = new House("zipCode", "Street", 55, 105);
+        US08DeactivateDeviceController myController = new US08DeactivateDeviceController(_myHouse);
+        DeviceDTO deviceToDeactivate = new DeviceDTO("myDevice","roomName1");
+        String expectedMessage = "Room name doesn't exist in the list";
+        //Act
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> myController.deactivateDevice (deviceToDeactivate));
         String actualMessage = exception.getMessage();
         //Assert
         assertEquals(expectedMessage, actualMessage);
