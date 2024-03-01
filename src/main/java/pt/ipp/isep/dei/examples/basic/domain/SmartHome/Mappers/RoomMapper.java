29dcb18f4e3a2272327f2aaa784d5f4bc2f2e805
Pedro Gil <1231851@isep.ipp.pt>
2024-03-01 17:12:06 +0000

    Rectified RoomMapper class

--- src/main/java/pt/ipp/isep/dei/examples/basic/domain/SmartHome/Mappers/RoomMapper.java
+++ src/main/java/pt/ipp/isep/dei/examples/basic/domain/SmartHome/Mappers/RoomMapper.java
@@ -1,7 +1,6 @@
 package pt.ipp.isep.dei.examples.basic.domain.SmartHome.Mappers;
 
 import pt.ipp.isep.dei.examples.basic.domain.SmartHome.DTO.RoomDTO;
-import pt.ipp.isep.dei.examples.basic.domain.SmartHome.Domain.House;
 import pt.ipp.isep.dei.examples.basic.domain.SmartHome.Domain.Room;
 
 import java.util.ArrayList;
@@ -10,26 +9,6 @@ import java.util.List;
 import java.util.Map;
 
 public class RoomMapper {
-    private Map<RoomDTO, Room> _roomsDTOAndRooms = new HashMap<>();
-    /**
-     * The house object that this mapper is associated with.
-     */
-    private House _house;
-
-    /**
-     * RoomDTOMapper constructor with parameter house.
-     *
-     * @param //house The house object that this mapper is associated with.
-     */
-    /*public RoomDTOMapper(House house) {
-        this._house = house;
-    }
-*/
-    // Converts a RoomDTO to a Room
-    public static Room room_DTOToDomain(RoomDTO roomDTO) throws InstantiationException {
-        return new Room(roomDTO.getName(), roomDTO.getHouseFloor(), roomDTO.getArea(), roomDTO.getHeight());
-    }
-
     // Converts a RoomDTO to a Room
     public static RoomDTO room_DomainToDTO(Room room) {
         return new RoomDTO(room.getRoomName(), room.getFloorNumber(), room.getArea(), room.getHeight());
@@ -52,7 +31,6 @@ public class RoomMapper {
         }
         return listOfRoomsInHouseDTO;
     }
-
     /**
      * This method maps a list of Room objects to a Map where the key is the room name and the value is the Room object.
      * It iterates over the list of rooms, and for each room, it adds an entry to the map with the room name as the key and the room object as the value.
@@ -60,19 +38,16 @@ public class RoomMapper {
      * @param rooms The list of Room objects to be mapped.
      * @return A Map with room names as keys and Room objects as values.
      */
-
     public static Map<String, Room> roomMap_NameAndDomain(List<Room> rooms) {
 
         Map<String, Room> roomsDTOAndRooms = new HashMap<>();
 
         rooms.forEach(room -> {
-            //RoomDTO roomDTO = RoomDTOMapper.room_DomainToDTO(room);
             roomsDTOAndRooms.put(room.getRoomName(), room);
         });
 
         return roomsDTOAndRooms;
     }
-
 }
 
 
